4ea4af2b006467d86f7724af14aa51cf12083aa5
Chand-28 <168813134+Chand-28@users.noreply.github.com>
2024-05-06 12:03:14 +0530

    c.js

--- c.js
+++ c.js
@@ -1,11 +1,11 @@
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 50) {
-      navbar.style.backgroundColor = '#fff'; // Change background color on scroll
-      navbar.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.1)'; // Add shadow on scroll
+      navbar.style.backgroundColor = '#fff'; 
+      navbar.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.1)'; 
     } else {
-      navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.8)'; // Reset background color
-      navbar.style.boxShadow = 'none'; // Remove shadow
+      navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.8)'; 
+      navbar.style.boxShadow = 'none'; 
     }
   });
   
